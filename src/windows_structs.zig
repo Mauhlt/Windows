15c9ec01c0df701345e79ca14913a60309a6f116
IdiotAtWork <bphilip777@gmail.com>
2026-06-04 03:33:10 -0400

    Updated struct defns

--- src/windows_structs.zig
+++ src/windows_structs.zig
@@ -3,7 +3,7 @@ const WndProc = @import("windows_fns.zig").WndProc;
 // Structs
 pub const WNDCLASSA = extern struct {
     style: u32 = 0,
-    wnd_proc: WndProc,
+    wnd_proc: ?WndProc,
     cb_cls_extra: i32 = 0,
     cb_wnd_extra: i32 = 0,
     instance: t.HINSTANCE,
@@ -15,7 +15,7 @@ pub const WNDCLASSA = extern struct {
 };
 pub const WNDCLASSEXA = extern struct {
     style: u32 = 0,
-    wnd_proc: WndProc,
+    wnd_proc: ?WndProc,
     cb_cls_extra: i32 = 0,
     cb_wnd_extra: i32 = 0,
     instance: t.HINSTANCE,
@@ -28,7 +28,7 @@ pub const WNDCLASSEXA = extern struct {
 };
 pub const WNDCLASSW = extern struct {
     style: u32 = 0,
-    wnd_proc: WndProc,
+    wnd_proc: ?WndProc,
     cls_extra: i32 = 0,
     wnd_extra: i32 = 0,
     instance: t.HINSTANCE,
@@ -41,7 +41,7 @@ pub const WNDCLASSW = extern struct {
 pub const WNDCLASSEXW = extern struct {
     size: u32 = @sizeOf(WNDCLASSEXW),
     style: u32 = 0,
-    wnd_proc: WndProc,
+    wnd_proc: ?WndProc,
     cls_extra: i32 = 0,
     wnd_extra: i32 = 0,
     instance: t.HINSTANCE,
